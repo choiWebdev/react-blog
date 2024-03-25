d7ce444517ae119915131257973cc4e01e74a0dc
choiWebdev <choi.webdev@gmail.com>
2024-03-26 01:30:30 +0900

    코딩애플 리액트로 블로그 만들기01

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, {useState} from 'react';
+import './App.css';
+
+function App() {
+  let [postSubject, setSubject] = useState(['글제목1', '글제목2', '글제목3']);
+  const [like, countLike] = useState(0);
+
+  function changeSubject (){
+    let newArray = [...postSubject];
+    newArray[0] = '여자 코트 추천';
+    setSubject(newArray);
+  }
+
+  return (
+    <div className="App">
+      <div className="bk-nav">
+        <h1>개발 Blog</h1>
+      </div>
+      <button onClick={ changeSubject }>버튼</button>
+      {
+        postSubject.map((e, idx) => {
+          return (
+          <div className="list">
+            <h4 className="">{postSubject[idx]} <span className="like" onClick={ () => {countLike(like + 1)} }>👍 {like}</span></h4>
+            <p>2024. 03. 18</p>
+          </div>
+          )
+        })
+      }
+    </div>
+  );
+}
+
+export default App;
